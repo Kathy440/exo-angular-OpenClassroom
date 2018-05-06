a5c7c5347d826d0c866f3a3ac1e0909bb0945c73
VOISIN Kathy <mkathy.voisin@gmail.com>
2018-05-06 18:34:25 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+
+    posts = [
+        {
+            title: 'Mon premier posts',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad aliquam animi\n' +
+            '    aspernatur, cumque deleniti ex expedita iste magni molestiae mollitia necessitatibus nesciunt\n' +
+            '    nulla quam quibusdam quidem sequi tempora voluptatum',
+            loveIts: 2,
+            created_at:  new Date()
+        },
+
+        {
+            title: 'Mon deuxième posts',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad aliquam animi\n' +
+            '    aspernatur, cumque deleniti ex expedita iste magni molestiae mollitia necessitatibus nesciunt\n' +
+            '    nulla quam quibusdam quidem sequi tempora voluptatum',
+            loveIts: -5,
+            created_at:  new Date()
+        },
+
+        {
+            title: 'Encore un posts',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad aliquam animi\n' +
+            '    aspernatur, cumque deleniti ex expedita iste magni molestiae mollitia necessitatibus nesciunt\n' +
+            '    nulla quam quibusdam quidem sequi tempora voluptatum',
+            loveIts: 0,
+            created_at:  new Date()
+
+        }
+
+        ];
+
+
+
 }
